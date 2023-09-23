0418e8deece66177d7e0e79e289980c03423b8f1
PThanapon <137179861+PThanapon@users.noreply.github.com>
2023-09-23 23:42:49 +0800

    Update e7supplementary.py

--- e7supplementary.py
+++ e7supplementary.py
@@ -8,21 +8,17 @@ warnings.simplefilter(action='ignore', category=FutureWarning)
 df = pd.read_csv("e7herodata.csv")
 
 stats = ['attack', 'health', 'defense', 'crit chance', 'crit damage', 'effectiveness', 'effectiveness resistance', 'speed']
-filter_columns = ["rarity", "class", "horoscope"]
+filter = ["rarity", "class", "horoscope"]
 
 for stat in stats:
-    fig, axes = plt.subplots(len(filter_columns), figsize=(8, 12))
+    fig, axes = plt.subplots(len(filter), figsize=(8, 12))
     
-    for i, fil in enumerate(filter_columns):
+    for i, fil in enumerate(filter):
         mean_df = df.groupby(fil)[stat].mean().reset_index()
 
         sorted_df = mean_df.sort_values(by=stat)
-        
-        if fil == "rarity":
-            rarity_order = sorted_df[fil]
-            sns.barplot(data=sorted_df, x=fil, y=stat, ax=axes[i], order=rarity_order)
-        else:
-            sns.barplot(data=sorted_df, x=fil, y=stat, ax=axes[i])
+
+        sns.barplot(data=sorted_df, x=fil, y=stat, ax=axes[i])
 
         axes[i].set_title(f"Average {stat} by {fil}")
         axes[i].set_xlabel(fil)
@@ -33,5 +29,11 @@ for stat in stats:
     plt.close()
 
 print("Plots saved as image files.")
+
+
+
+
+
+
 
 
